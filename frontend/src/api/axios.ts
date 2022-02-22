b3c9750dc2c3b9a4173b56fc5999bd4d595ca63f
Watremons <z2331355763@sina.com>
2022-02-22 17:15:15 +0800

    ✨ feat(frontend): add axios并进行初步封装

---
+++ frontend/src/api/axios.ts
@@ -0,0 +1,64 @@
+import axios, { AxiosRequestConfig } from "axios";
+
+export interface McAxiosRequestConfig extends AxiosRequestConfig {
+  extraConfig?: {
+    tokenRetryCount: number; // 标记当前请求的csrf token重试次数
+  };
+}
+
+const timeout = 60000; // 请求超时时间和延迟请求超时时间统一设置
+
+const config: McAxiosRequestConfig = {
+  // baseURL: (import.meta.env.VITE_BASE_URL as string) || "/",
+  baseURL: '127.0.0.1:8000/',
+  timeout,
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+const instance = axios.create(config);
+
+instance.interceptors.request.use(async (config: McAxiosRequestConfig) => {
+  if (!config.extraConfig?.tokenRetryCount) {
+    config.extraConfig = {
+      tokenRetryCount: 0,
+    };
+  }
+  return config;
+});
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (err) => {
+    if (axios.isCancel(err)) {
+      // 取消的请求，不报错
+      return;
+    }
+
+    if (err.message === "Network Error") {
+      return;
+    }
+    if (err.message.includes("timeout")) {
+      return;
+    }
+    if (err.response?.status >= 500) {
+      return;
+    }
+
+    const { data: responseData } = err.response || {};
+    const { status } = responseData || {};
+    // 判断状态码决定结果
+    if (status) {
+      switch (parseInt(status)) {
+        default:
+          break;
+      }
+    }
+    return err.response;
+  }
+);
+
+export default instance;
---
+++ frontend/src/api/request.ts
@@ -0,0 +1,51 @@
+import instance, { McAxiosRequestConfig } from './axios';
+import { AxiosInstance, AxiosResponse } from 'axios';
+
+type RequestMethod = 'get' | 'post';
+class Request {
+  
+  private instance: AxiosInstance;
+
+  constructor(instance: AxiosInstance) {
+    this.instance = instance;
+  }
+
+  /**
+   * @description: get请求，参数同axios的get方法，额外增加showLoading参数用于控制全局loading状态
+   * @param {string} url
+   * @param {McAxiosRequestConfig} config
+   * @return Promise<AxiosResponse<any>>
+   */
+  public get(
+    url: string,
+    config: McAxiosRequestConfig = {}
+  ): Promise<AxiosResponse<any>> {
+    return this.request('get', url, config);
+  }
+
+  /**
+   * @description: post请求，参数同axios的post方法，额外增加showLoading参数用于控制全局loading状态
+   * @param {string} url
+   * @param {McAxiosRequestConfig} config
+   * @return Promise<AxiosResponse<any>>
+   */
+  public post(
+    url: string,
+    data: any = {},
+    config: McAxiosRequestConfig = {}
+  ): Promise<AxiosResponse<any>> {
+    return this.request('post', url, config, data);
+  }
+
+  private async request(
+    method: RequestMethod,
+    url: string,
+    config: McAxiosRequestConfig,
+    ...params: any
+  ) {
+    const res = await this.instance[method](url, ...params, config);
+    return res;
+  }
+}
+
+export const request = new Request(instance);
