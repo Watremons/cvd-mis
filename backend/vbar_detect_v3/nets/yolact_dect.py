095bfc3bd3cb264fe226e57357067c72d2e730e2
Watremons <z2331355763@sina.com>
2022-03-11 15:00:35 +0800

    ✨ feat(backend): 新增模型相关文件

---
+++ backend/vbar_detect_v3/nets/yolact_dect.py
@@ -0,0 +1,188 @@
+from config import COLORS, cfg, set_cfg, args
+from nets.yolact import Yolact
+from utils.augmentations import FastBaseTransform
+from utils import timer
+from layers.output_utils import postprocess, undo_image_transformation
+import torch
+import torch.backends.cudnn as cudnn
+from utils.functions import SavePath
+
+import cv2
+from collections import defaultdict
+import numpy as np
+import warnings
+
+color_cache = defaultdict(lambda: {})
+
+
+def prep_display(dets_out, img, h, w, undo_transform=True, class_color=False, mask_alpha=0.45, fps_str=''):
+    """
+    Note: If undo_transform=False then im_h and im_w are allowed to be None.
+    """
+    if undo_transform:
+        img_numpy = undo_image_transformation(img, w, h)
+        img_gpu = torch.Tensor(img_numpy).cuda()
+    else:
+        img_gpu = img / 255.0
+        h, w, _ = img.shape
+
+    with timer.env('Postprocess'):
+        save = cfg.rescore_bbox
+        cfg.rescore_bbox = True
+        t = postprocess(dets_out, w, h, visualize_lincomb=args.display_lincomb,
+                        crop_masks=args.crop,
+                        score_threshold=args.score_threshold)
+        cfg.rescore_bbox = save
+
+    with timer.env('Copy'):
+        idx = t[1].argsort(0, descending=True)[:args.top_k]
+
+        if cfg.eval_mask_branch:
+            # Masks are drawn on the GPU, so don't copy
+            masks = t[3][idx]
+        classes, scores, boxes = [x[idx].cpu().numpy() for x in t[:3]]
+
+    num_dets_to_consider = min(args.top_k, classes.shape[0])
+    for j in range(num_dets_to_consider):
+        if scores[j] < args.score_threshold:
+            num_dets_to_consider = j
+            break
+
+    # Quick and dirty lambda for selecting the color for a particular index
+    # Also keeps track of a per-gpu color cache for maximum speed
+    def get_color(j, on_gpu=None):
+        global color_cache
+        color_idx = (classes[j] * 5 if class_color else j * 5) % len(COLORS)
+
+        if on_gpu is not None and color_idx in color_cache[on_gpu]:
+            return color_cache[on_gpu][color_idx]
+        else:
+            color = COLORS[color_idx]
+            if not undo_transform:
+                # The image might come in as RGB or BRG, depending
+                color = (color[2], color[1], color[0])
+            if on_gpu is not None:
+                color = torch.Tensor(color).to(on_gpu).float() / 255.
+                color_cache[on_gpu][color_idx] = color
+            return color
+
+    # First, draw the masks on the GPU where we can do it really fast
+    # Beware: very fast but possibly unintelligible mask-drawing code ahead
+    # I wish I had access to OpenGL or Vulkan but alas, I guess Pytorch tensor operations will have to suffice
+    if args.display_masks and cfg.eval_mask_branch and num_dets_to_consider > 0:
+        # After this, mask is of size [num_dets, h, w, 1]
+        masks = masks[:num_dets_to_consider, :, :, None]
+
+        # Prepare the RGB images for each mask given their color (size [num_dets, h, w, 1])
+        colors = torch.cat(
+            [get_color(j, on_gpu=img_gpu.device.index).view(1, 1, 1, 3) for j in range(num_dets_to_consider)], dim=0)
+        masks_color = masks.repeat(1, 1, 1, 3) * colors * mask_alpha
+
+        # This is 1 everywhere except for 1-mask_alpha where the mask is
+        inv_alph_masks = masks * (-mask_alpha) + 1
+
+        # I did the math for this on pen and paper. This whole block should be equivalent to:
+        #    for j in range(num_dets_to_consider):
+        #        img_gpu = img_gpu * inv_alph_masks[j] + masks_color[j]
+        masks_color_summand = masks_color[0]
+        if num_dets_to_consider > 1:
+            inv_alph_cumul = inv_alph_masks[:(num_dets_to_consider - 1)].cumprod(dim=0)
+            masks_color_cumul = masks_color[1:] * inv_alph_cumul
+            masks_color_summand += masks_color_cumul.sum(dim=0)
+        img_gpu = torch.ones_like(img_gpu)
+        img_gpu = img_gpu * inv_alph_masks.prod(dim=0) + masks_color_summand
+        # frame = cv2.cvtColor(np.array((img_gpu * inv_alph_masks.prod(dim=0)).cpu()), cv2.COLOR_RGB2BGR)
+        # cv2.imshow("mask", frame)
+        # cv2.waitKey()
+    if args.display_fps:
+        # Draw the box for the fps on the GPU
+        font_face = cv2.FONT_HERSHEY_DUPLEX
+        font_scale = 0.6
+        font_thickness = 1
+
+        text_w, text_h = cv2.getTextSize(fps_str, font_face, font_scale, font_thickness)[0]
+
+        img_gpu[0:text_h + 8, 0:text_w + 8] *= 0.6  # 1 - Box alpha
+
+    # Then draw the stuff that needs to be done on the cpu
+    # Note, make sure this is a uint8 tensor or opencv will not anti alias text for whatever reason
+    img_numpy = (img_gpu * 255).byte().cpu().numpy()
+
+    if args.display_fps:
+        # Draw the text on the CPU
+        text_pt = (4, text_h + 2)
+        text_color = [255, 255, 255]
+
+        cv2.putText(img_numpy, fps_str, text_pt, font_face, font_scale, text_color, font_thickness, cv2.LINE_AA)
+
+    if num_dets_to_consider == 0:
+        return img_numpy
+
+    if args.display_text or args.display_bboxes:
+        for j in reversed(range(num_dets_to_consider)):
+            x1, y1, x2, y2 = boxes[j, :]
+            color = get_color(j)
+            score = scores[j]
+
+            if args.display_bboxes:
+                cv2.rectangle(img_numpy, (x1, y1), (x2, y2), color, 1)
+
+            if args.display_text:
+                _class = cfg.dataset.class_names[classes[j]]
+                text_str = '%s: %.2f' % (_class, score) if args.display_scores else _class
+
+                font_face = cv2.FONT_HERSHEY_DUPLEX
+                font_scale = 0.6
+                font_thickness = 1
+
+                text_w, text_h = cv2.getTextSize(text_str, font_face, font_scale, font_thickness)[0]
+
+                text_pt = (x1, y1 - 3)
+                text_color = [255, 255, 255]
+
+                cv2.rectangle(img_numpy, (x1, y1), (x1 + text_w, y1 - text_h - 4), color, -1)
+                cv2.putText(img_numpy, text_str, text_pt, font_face, font_scale, text_color, font_thickness,
+                            cv2.LINE_AA)
+
+    return img_numpy
+
+
+def detect_image(net: Yolact, image):
+    with torch.no_grad():
+        net.detect.use_fast_nms = args.fast_nms
+        net.detect.use_cross_class_nms = args.cross_class_nms
+        cfg.mask_proto_debug = args.mask_proto_debug
+
+        frame = torch.from_numpy(np.array(image)).cuda().float()
+        batch = FastBaseTransform()(frame.unsqueeze(0))
+        preds = net(batch)
+
+        img_numpy = prep_display(preds, frame, None, None, undo_transform=False)
+
+        img_numpy = img_numpy[:, :, (2, 1, 0)]
+
+    return img_numpy
+
+
+def YOLACT():
+    model_path = SavePath.from_str(args.trained_model)
+    args.config = model_path.model_name + '_config'
+    # print('Config not specified. Parsed %s from the file name.\n' % args.config)
+    set_cfg(args.config)
+    if args.cuda:
+        cudnn.fastest = True
+        torch.set_default_tensor_type('torch.cuda.FloatTensor')
+
+    print('[Info] Load Yolact Model...')
+    warnings.filterwarnings("ignore")
+    net = Yolact()
+    net.load_weights(args.trained_model)
+    net.eval()
+    net = net.cuda()
+    print('[Info] Load Yolact Done!')
+
+    return net
+
+
+
+
---
+++ backend/vbar_detect_v3/run_video_detect.py
@@ -0,0 +1,198 @@
+import time
+import os
+import cv2
+import numpy as np
+from PIL import Image
+
+from pyheatmap.heatmap import HeatMap
+
+from nets.yolox import YOLO
+from nets import yolact_dect
+from nets.yolact_dect import YOLACT
+
+
+def distance(point1, point2):
+    differ = (point1[0] - point2[0])**2 + (point1[1] - point2[1])**2
+    return differ**0.5
+
+
+def vibrate_point_dect(segm_image, cut_images_locate, pic_index):
+    gray = cv2.cvtColor(segm_image, cv2.COLOR_BGR2GRAY)
+    gray[0][:] = 255
+    gray[-1][:] = 255
+    gray[:][0] = 255
+    gray[:][-1] = 255
+    binary, contours, hierarchy = cv2.findContours(gray, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    area = []
+    # 找到最大的轮廓
+    for k in range(len(contours)):
+        area.append(cv2.contourArea(contours[k]))
+    max_idx = np.argmax(np.array(area))
+    area[max_idx] = 0
+    max_idx = np.argmax(np.array(area))
+    area[max_idx] = 0
+    second_idx = np.argmax(np.array(area))
+
+    out_contours = np.concatenate((contours[max_idx], contours[second_idx]), axis=0)
+    rect = cv2.minAreaRect(out_contours)
+
+    points = cv2.boxPoints(rect)
+    points = np.int0(points)
+
+    points = points.tolist()
+    max_point = max([points[0][1], points[1][1], points[2][1], points[3][1]])
+    p1 = list()
+    for i in range(4):
+        if points[i][1] == max_point:
+            p1 = points[i]
+            p1_id = i
+    points.pop(p1_id)
+    min_dist = (points[0][0] - p1[0]) ** 2 + (points[0][1] - p1[1]) ** 2
+    p2 = points[0]
+
+    for i in range(len(points)):
+        dist = (points[i][0] - p1[0]) ** 2 + (points[i][1] - p1[1]) ** 2
+        if min_dist > dist:
+            p2 = points[i]
+    vp = [int((p1[0] + p2[0]) / 2 + cut_images_locate[pic_index][0]),
+          int((p1[1] + p2[1]) / 2 + cut_images_locate[pic_index][1])]
+
+    threshold = distance(p1, p2)
+
+    return vp, threshold
+
+
+def apply_heatmap(image, data):
+    """
+    Create heatMap image
+
+    :param image: raw pic as background
+    :param data: an array of number pair as point
+    :returns: image with heatmap
+    :raises IOError: save pic or read pic error
+    """
+    # create a new pic from raw image, which background is black
+    background = Image.new("RGB", (image.shape[1], image.shape[0]), color=0)
+    # create Heatmap
+    hm = HeatMap(data)
+    hit_img = hm.heatmap(base=background, r=35)  # base: 背景图片，r: 热力半径，默认为10
+    hit_img = cv2.cvtColor(np.asarray(hit_img), cv2.COLOR_RGB2BGR)  # Image格式转换成cv2格式
+    overlay = image.copy()
+    alpha = 0.4
+    cv2.rectangle(overlay, (0, 0), (image.shape[1], image.shape[0]), (255, 0, 0), -1)  # 设置蓝色为热度图基本色蓝色
+    image_with_base = cv2.addWeighted(overlay, alpha, image, 1-alpha, 0)  # 将背景热度图覆盖到原图
+    image_with_heatmap = cv2.addWeighted(hit_img, alpha, image_with_base, 1-alpha, 0)  # 将热度图覆盖到原图
+    return image_with_heatmap
+
+
+def point2str(point):
+    """
+    Transform number pair to str
+
+    :param point: number pair
+    :returns: image with heatmap
+    :raises IOError: save pic or read pic error
+    """
+    return "[{0},{1}]\n".format(point[0], point[1])
+
+
+def video_vibrate_detect(project_name: str, video_path: str):
+    """
+    do video_vibrate_detect
+
+    :param project_name:  name of detection project
+    :returns: total frame num and total point num
+    :raises IOError: save pic or read pic error
+    """
+    yolo = YOLO()
+    yolact = YOLACT()
+    print("[Info] {0} video detect start!".format(project_name))
+    # 检查目标目录是否存在，并创建
+    # [1]参数1：数据文件保存路径
+    out_dir = os.path.join('.', 'out_images', project_name)
+    if not os.path.isdir(out_dir):
+        os.mkdir(out_dir)
+    # 调用摄像头，若有参数则改为获取参数指定的视频文件
+    capture = cv2.VideoCapture(video_path)
+    # capture=cv2.VideoCapture(0)
+
+    # 获取原始视频帧率及总帧数
+    # print("FPS = {0}".format(capture.get(cv2.CAP_PROP_FPS)))
+    # print("Frame Number = {0}".format(capture.get(cv2.CAP_PROP_FRAME_COUNT)))
+
+    fourcc = cv2.VideoWriter_fourcc(*"XVID")
+    out = cv2.VideoWriter(os.path.join(out_dir, '{0}_result.avi'.format('test')), fourcc, 25, (600, 584))
+
+    fps = 0.0
+    points = []
+    point = None
+    raw_frame = None
+
+    frame_count = 0
+
+    # while (True): #调用摄像头
+    while capture.isOpened():  # 读取视频文件
+        t1 = time.time()
+        # 读取某一帧, 返回ref表示是否正确读取，返回frame表示本帧的BGR格式图像，frame.shape = (640,480,3)
+        ref, frame = capture.read()
+        if raw_frame is None:
+            # 保存起始帧用于热力图绘制
+            raw_frame = frame
+            # 保存起始的原始图片
+            cv2.imwrite(os.path.join(out_dir, 'raw.jpg'), frame)
+
+        # 格式转变，BGRtoRGB
+        try:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        except Exception as e:
+            # print(e.args)  # 打印错误信息
+            capture.release()  # 释放读取的文件
+            break
+
+        # 转变成Image
+        frame = Image.fromarray(np.uint8(frame))
+
+        # 1. 使用yolo进行振捣棒位置识别并对振捣棒截图
+        # function: 返回值为原始图像，切割出的振捣棒，标注振捣棒的原图
+        _, cut_images, cut_images_locate = yolo.detect_image(frame)
+        for index in range(len(cut_images)):
+            img = cut_images[index]
+            # 2. 使用yolact将1得到的截图中的振捣棒识别
+            segm_image = yolact_dect.detect_image(yolact, img)
+            # 3. 使用振捣棒点位识别函数从截图中得到振捣棒底部点坐标
+            point, threshold = vibrate_point_dect(segm_image, cut_images_locate, index)
+
+        # RGBtoBGR满足opencv显示格式
+        frame = cv2.cvtColor(np.array(frame), cv2.COLOR_RGB2BGR)
+        # 帧率取起始和当前时间间隔
+        fps = (fps + (1./(time.time()-t1))) / 2
+        # 在当前帧左上角设置帧率显示
+        frame = cv2.putText(frame, "fps= %.2f" % (fps), (0, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        # 在当前帧显示点位坐标
+        if point is not None:
+            cv2.circle(frame, (point[0], point[1]), 5, (0, 255, 255), -1, 0, 0)
+            frame = cv2.putText(frame, "[{}, {}]".format(point[0], point[1]), (point[0], point[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            points.append(point)
+
+        cv2.imshow("video", frame)
+        out.write(frame)
+        cv2.imwrite(os.path.join(out_dir, "{:05d}.jpg".format(frame_count)), frame)
+        frame_count += 1
+
+        c = cv2.waitKey(1) & 0xff
+        if c == 27:
+            capture.release()
+            break
+    # 后端绘制热力图
+    image_with_heatmap = apply_heatmap(raw_frame, points)
+    cv2.imwrite(os.path.join(out_dir, "heatmap.jpg"), image_with_heatmap)
+    # 保存点坐标到文本文件，改为前端渲染
+    fo = open(os.path.join(out_dir, 'points.txt'), "w")
+    fo.writelines(map(point2str, points))
+    fo.close()
+    print("[Info] {0} Finished!".format(project_name))
+    return frame_count, len(points)
+
+
+if __name__ == '__main__':
+    video_vibrate_detect('test', './test.mp4')
