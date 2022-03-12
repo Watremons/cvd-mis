047390b6d7f352b555573435be80a2c061609b1c
Watremons <z2331355763@sina.com>
2022-03-12 19:00:52 +0800

    🐞 fix(backend): 修复models问题

--- backend/misback/models.py
+++ backend/misback/models.py
@@ -31,10 +31,9 @@ class User(models.Model):
     uid = models.AutoField(db_column='uid', null=False, primary_key=True, verbose_name='用户id')
     userName = models.CharField(db_column='user_name', max_length=20, default='游客用户', null=False, verbose_name='用户名')
     userDes = models.CharField(db_column='user_des', max_length=50, default='这个人很懒，没有留下简介', null=False, verbose_name='用户简介')
-    projectNum = models.IntegerField(db_column='project_num', default=0, null=False, verbose_name='用户拥有的检测项目数')
     createDate = models.DateField(db_column='create_date', null=False, auto_now_add=True, verbose_name='用户注册时间')
     authority = models.IntegerField(db_column='authority', default=0, null=False, choices=AUTH_TYPE, verbose_name='用户权限')
-    description = models.CharField(db_column='description', null=True, verbose_name='备注')
+    description = models.CharField(db_column='description', max_length=50, null=True, verbose_name='备注')
 
     class Meta:
         db_table = 'user'
@@ -60,7 +59,7 @@ class Project(models.Model):
     videoFile = models.CharField(db_column='video_file', max_length=50, null=False, verbose_name="检测项目文件名")
     projectStatus = models.IntegerField(db_column='project_status', null=False, default=0, choices=STATUS_TYPE, verbose_name="检测项目当前状态, 0未启动, 1进行中, 2已完成")
     uid = models.ForeignKey(User, models.CASCADE, db_column='uid', null=False, verbose_name="检测项目所属用户")
-    description = models.CharField(db_column='description', null=True, verbose_name='备注')
+    description = models.CharField(db_column='description', max_length=50, null=True, verbose_name='备注')
 
     class Meta:
         db_table = 'project'
