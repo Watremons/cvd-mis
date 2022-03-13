579378dacd262c6fd9c9cca9f27c7dd33fb26a61
Watremons <z2331355763@sina.com>
2022-03-13 14:50:50 +0800

    ✨ feat(backend): 新增媒体文件media路径和url引用

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-# from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework.documentation import include_docs_urls
 
 
@@ -25,5 +26,4 @@ urlpatterns = [
     path('docs/', include_docs_urls(title="apisDocs")),
 ]
 
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
