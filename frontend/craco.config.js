c0010bf60c1b89a5bb3705e6a25fefa71e987576
Watremons <z2331355763@sina.com>
2022-03-28 21:05:30 +0800

    ✨ feat(frontend): 新增craco配置文件
    为引入的craco打包新增配置文件，包括less-loader, 本地代理和babel按需引入工具

---
+++ frontend/craco.config.js
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const CracoLessPlugin = require('craco-less');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: { '@primary-color': '#1DA57A' },
+            modules: true,
+            javascriptEnabled: true
+          }
+        }
+      }
+    }
+  ],
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000/',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  },
+  babel: {
+    plugins: [
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
+      [
+        'import',
+        {
+          libraryName: 'antd',
+          libraryDirectory: 'es',
+          style: true
+        }
+      ]
+    ]
+  }
+};
