4809c810faf0d157474aaedb56180dcb805ac8fe
Watremons <z2331355763@sina.com>
2022-03-28 21:15:32 +0800

    ✨ feat(frontend): 新增项目页面

---
+++ frontend/src/pages/project/ProjectPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function ProjectPage() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>ProjectPage</h1>
+        <p>计划页面</p>
+      </header>
+    </div>
+  );
+}
