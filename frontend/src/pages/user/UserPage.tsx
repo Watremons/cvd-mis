be97b230cd1a440184b77d2c3ea8f5ee67d8acd5
Watremons <z2331355763@sina.com>
2022-03-28 21:16:00 +0800

    ✨ feat(frontend): 新增用户页

---
+++ frontend/src/pages/user/UserPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function UserPage() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>UserPage</h1>
+        <p>用户页面</p>
+      </header>
+    </div>
+  );
+}
