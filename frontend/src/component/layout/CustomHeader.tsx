01e9788b4af810f76dc878d0feace972ea5fd98b
Watremons <z2331355763@sina.com>
2022-04-22 16:12:44 +0800

    🐞 fix(frontend): 修复不同分辨率下下拉框变形问题

--- frontend/src/component/layout/CustomHeader.tsx
+++ frontend/src/component/layout/CustomHeader.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Dropdown, Layout, Menu, message } from 'antd';
-import { LogoutOutlined } from '@ant-design/icons';
+import { DownOutlined, LogoutOutlined } from '@ant-design/icons';
 
 import logo from '../../logo.svg';
 import './CustomHeader.less';
@@ -45,7 +45,13 @@ export default function CustomHeader() {
         混凝土振捣检测系统
       </div>
       <Menu theme="dark" mode="horizontal"></Menu>
-      <Dropdown.Button className="dropdown" overlay={dropdownMenu} placement="bottom">
+      <Dropdown.Button
+        className="dropdown"
+        overlay={dropdownMenu}
+        placement="bottomRight"
+        icon={<DownOutlined />}
+        onClick={() => navigate('user/user-setting', { replace: true })}
+      >
         {userName ?? '未知用户'}
       </Dropdown.Button>
     </Header>
